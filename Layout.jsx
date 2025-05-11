c186cacc7116cf0c86e110806e1382ca2bc89879
NileshKumarPU <codemate18@gmail.com>
2025-05-11 19:54:33 +0530

    added vercel analytics.

--- Layout.jsx
+++ Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Footer, Header } from "./src/components";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Layout(){
     return(
