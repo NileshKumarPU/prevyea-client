14d268528993cad49a66a39d317077677970feb8
Nilesh <codemate18@gmail.com>
2025-07-07 00:18:11 +0530

    responsive profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ export default function Profile(props){
     return(
 
         <>
-        <div className="rounded-lg shadow-lg p-4 min-w-md m-auto bg-white">
+        <div className="rounded-lg shadow-lg p-4 max-w-md m-auto bg-white">
         <p>Username : {props.username}</p>
         <p>Full name : {props.fullname}</p>
         <p>Email    : {props.email}</p>
