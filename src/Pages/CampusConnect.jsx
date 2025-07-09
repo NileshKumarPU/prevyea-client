cb363b98e537ada88979bdf5eb6316abbba774cb
Nilesh <codemate18@gmail.com>
2025-07-09 17:01:44 +0530

    micro upload

--- src/Pages/CampusConnect.jsx
+++ src/Pages/CampusConnect.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
-export default function CampusConnect(){
-
-  const cookies = useCookies()
+export default function CampusConnect() {
+  const cookies = useCookies();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -23,36 +22,28 @@ export default function CampusConnect(){
     verifyCookie();
   }, [cookies]);
 
-
-
-
-
-
-
-
-  
-const books = [
-  {
-    title: "CC-3: Available",
-    pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1751502947/PrevYea_CC3_Micro_gp1rki.pdf",
-  },
-  {
-    title: "CC-4: Available",
-    pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1751658638/PrevYea_CC4_Micro_loyzok.pdf",
-  },
-  {
-    title: "GE-2: Available",
-    pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1751926571/PrevYea_GE2_Micro_rnytsi.pdf",
-  },
-  {
-    title: "AECC-2 Awaited",
-    pdf: "https://example.com/design-patterns.pdf",
-  },
-
-];
-
-
-
+  const books = [
+    {
+      title: "CC-3: Java",
+      pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1751502947/PrevYea_CC3_Micro_gp1rki.pdf",
+      uploadtime: " 06:05 3-July",
+    },
+    {
+      title: "CC-4: Arch. ",
+      pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1751658638/PrevYea_CC4_Micro_loyzok.pdf",
+      uploadtime: " 01:20 5-July",
+    },
+    {
+      title: "GE-2: Maths",
+      pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1751926571/PrevYea_GE2_Micro_rnytsi.pdf",
+      uploadtime: " 03:46 8-July",
+    },
+    {
+      title: "AECC-2: EVS",
+      pdf: "https://res.cloudinary.com/dwpc2rgdn/image/upload/v1752060394/PrevYea_AECC2_Micro_fde1xp.pdf",
+      uploadtime: " 9-July 16:56",
+    },
+  ];
 
   return (
     <div className="mt-8" style={{ padding: "2rem", minHeight: "100vh" }}>
@@ -70,36 +61,62 @@ const books = [
               borderRadius: "8px",
               boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
             }}
-            >
-            <span style={{ fontSize: "1.2rem", fontWeight: "bold" }}>{book.title}</span>
-            {isLoggedIn ?
-            <a
-            href={book.pdf}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              display: "inline-block",
-              marginLeft: "1rem",
-              backgroundColor: "#2CB8C6",
-              color: "#ffffff",
-              padding: "0.5rem 1rem",
-              borderRadius: "5px",
-              textDecoration: "none",
-            }}
-            >
-              Download PDF
-            </a>
+          >
+            <div className="flex items-center">
+              <div>
+                <div>
+                  <span style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+                    {book.title}
+                  </span>
+                </div>
+                <div>
+                  <span
+                    style={{
+                      fontSize: "0.75rem",
+                      // marginLeft: "1rem",
+                      color: "gray",
+                      fontStyle: "italic",
+                    }}
+                  >
+                    (Uploaded:{book.uploadtime})
+                  </span>
+                </div>
+              </div>
 
-            :(
-              <span style={{ marginLeft: "1rem", color: "gray", fontStyle: "italic" }}>
-                (Login to download)
-              </span>
-            )}
-        
+              <div>
+                {isLoggedIn ? (
+                  <a
+                    href={book.pdf}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      display: "inline-block",
+                      marginLeft: "1.2rem",
+                      backgroundColor: "#2CB8C6",
+                      color: "#ffffff",
+                      padding: "0.5rem 1rem",
+                      borderRadius: "5px",
+                      textDecoration: "none",
+                    }}
+                  >
+                    Download PDF
+                  </a>
+                ) : (
+                  <span
+                    style={{
+                      marginLeft: "1.2rem",
+                      color: "gray",
+                      fontStyle: "italic",
+                    }}
+                  >
+                    (Login to download)
+                  </span>
+                )}
+              </div>
+            </div>
           </li>
         ))}
       </ul>
     </div>
   );
-};
-
+}
