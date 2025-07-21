df82b5eda7bdbb1e29c0b77e3ad2d06bd6dd14dc
Nilesh <codemate18@gmail.com>
2025-07-21 16:14:39 +0530

    signup page

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { AuthOTP } from "../components";
-import { toast,ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import { HashLoader } from "react-spinners";
 
 export default function Signup() {
@@ -11,22 +11,22 @@ export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [admin, setAdmin] = useState(false);
-  const [showAuth,setShowAuth] = useState(false)
-  const [loading,setLoading] = useState(false);
+  const [showAuth, setShowAuth] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-   const isValidGmail = (email) => {
+  const isValidGmail = (email) => {
     const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
     return gmailRegex.test(email);
   };
 
   const user = {
-    username:username,
-    fullname:fullname,
-    email:email,
-    password:password,
-    admin:admin
-  }
+    username: username,
+    fullname: fullname,
+    email: email,
+    password: password,
+    admin: admin,
+  };
   const admin_ref = useRef(null);
   const user_ref = useRef(null);
 
@@ -36,233 +36,138 @@ export default function Signup() {
 
     if (!isValidGmail(email)) {
       toast.error("Please enter a valid Gmail address.");
-      setLoading(false)
-      return
+      setLoading(false);
+      return;
     }
-    
+
     const result = await axios({
-        
-        method:`post`,
-        url: `https://prevyea-server.vercel.app/otp`,
-        data:{
-            email:email
-        }
-    })
-    
-    
+      method: `post`,
+      url: `https://prevyea-server.vercel.app/otp`,
+      data: {
+        email: email,
+      },
+    });
+
     setLoading(false);
-    const {success,message} = result.data
-    if(success){
-    setShowAuth(success); // Shows screen for entering otp.
-    toast.success(message);
-    }else{
-    toast(message,{position:"bottom-right"});
+    const { success, message } = result.data;
+    if (success) {
+      setShowAuth(success); // Shows screen for entering otp.
+      toast.success(message);
+    } else {
+      toast(message, { position: "bottom-right" });
     }
-    
   };
-
-
-
-//   useEffect(() => {
-//     if(!showAuth){
-
-//         if (admin) {
-//             admin_ref.current.classList.add("bggreen");
-//             user_ref.current.classList.remove("bggreen");
-//         } else {
-//             user_ref.current.classList.add("bggreen");
-//             admin_ref.current.classList.remove("bggreen");
-//         }
-//     }
-//     });
 
   return (
     <>
       <div className="flex justify-center w-4/5 m-auto mt-4 mb-4 min-h-screen">
-      <ToastContainer/>
-      {showAuth ? 
-      <AuthOTP userData={user}/>
-      :
+        <ToastContainer />
+        {showAuth ? (
+          <AuthOTP userData={user} />
+        ) : (
+          <div className=" p-6 bg-amber-50 min-h-screen ">
+            <div className="max-w-md mx-auto p-6 bggreen shadow-md rounded-xl min-w-sm ">
+              <h2 className="text-2xl font-bold mb-6 text-center">
+                User SignUp
+              </h2>
+              <form onSubmit={handleSignup}>
+                <div className="mb-4 ">
+                  <label className="block  ">
+                    Username<span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
+                    value={username}
+                    placeholder="abcd34"
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-4 ">
+                  <label className="block  ">
+                    Fullname<span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
+                    value={fullname}
+                    placeholder="John Doe"
+                    onChange={(e) => setfullname(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-4 ">
+                  <label className="block  ">
+                    Email<span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="email"
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
+                    value={email}
+                    placeholder="abcd@xyz.com"
+                    onChange={(e) => setEmail(e.target.value.toLowerCase())}
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block  ">
+                    Password<span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    type="password"
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
+                    value={password}
+                    placeholder="Abc@123"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="w-full my-2 rounded-lg flex justify-between">
+                  <div className="w-1/2 border rounded-lg ">
+                    <button
+                      className="w-full rounded-lg px-4 py-2 bgblue"
+                      ref={user_ref}
+                      onClick={() => {
+                        setAdmin(false);
+                      }}
+                    >
+                      {" "}
+                      User
+                    </button>
+                  </div>
 
+                  <div className="w-1/2 border rounded-lg ">
+                    <button
+                      className="w-full rounded-lg px-4 py-2 "
+                      ref={admin_ref}
+                      onClick={() => {
+                        alert("Not allowed to Signup as Admin");
+                      }}
+                    >
+                      Admin
+                    </button>
+                  </div>
+                </div>
+                {loading ? (
+                  <div className="flex justify-center">
+                    {" "}
+                    <HashLoader />
+                  </div>
+                ) : (
+                  <button
+                    type="submit"
+                    className="w-full outline-1  py-2 rounded-lg  "
+                  >
+                    SignUp
+                  </button>
+                )}
 
-       <div className=" p-6 bg-amber-50 min-h-screen ">
-        <div className="max-w-md mx-auto p-6 bggreen shadow-md rounded-xl min-w-sm ">
-          <h2 className="text-2xl font-bold mb-6 text-center">
-            User SignUp
-          </h2>
-          <form onSubmit={handleSignup}>
-            <div className="mb-4 ">
-              <label className="block  ">Username<span className="text-red-500">*</span></label>
-              <input
-                type="text"
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-                value={username}
-                placeholder="abcd34"
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-4 ">
-              <label className="block  ">Fullname<span className="text-red-500">*</span></label>
-              <input
-                type="text"
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-                value={fullname}
-                placeholder="John Doe"
-                onChange={(e) => setfullname(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-4 ">
-              <label className="block  ">Email<span className="text-red-500">*</span></label>
-              <input
-                type="email"
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-                value={email}
-                placeholder="abcd@xyz.com"
-                onChange={(e) => setEmail(e.target.value.toLowerCase())}
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label className="block  ">Password<span className="text-red-500">*</span></label>
-              <input
-                type="password"
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-                value={password}
-                placeholder="Abc@123"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="w-full my-2 rounded-lg flex justify-between">
-           <div className="w-1/2 border rounded-lg ">
-             <button
-                className="w-full rounded-lg px-4 py-2 bgblue"
-                ref={user_ref}
-                onClick={() => {
-                    setAdmin(false);
-                }}
-                >
-                {" "}
-                User
-              </button>
-            </div>
-
-            <div className="w-1/2 border rounded-lg ">
-              <button
-                className="w-full rounded-lg px-4 py-2 "
-                ref={admin_ref}
-                onClick={() => {
-                    alert("Not allowed to Signup as Admin");
-                }}
-                >
-                Admin
-              </button>
+                <ToastContainer />
+              </form>
             </div>
           </div>
-          {loading? <div className="flex justify-center"> <HashLoader/>
-            </div> :
-            <button
-            type="submit"
-            className="w-full outline-1  py-2 rounded-lg hover:bg-blue-700 transition duration-300 "
-            >
-              SignUp
-            </button>
-            }
-            
-            <ToastContainer />
-          </form>
-        </div>
-      </div>
-        // <div >
-
-        
-        // <form onSubmit={handleSignup} className="border-2 rounded-lg p-12 mt-10 bggreen text-black max-w-md">
-        //   <h1 className="text-center text-2xl font-bold mb-6">User SignUp </h1>
-        //   <div>
-        //     <label className="block">Username<span className="text-red-500">*</span></label>
-        //     <input
-        //       type="text"
-             
-        //       required
-        //       value={username}
-        //       onChange={(e) => {
-        //           setUsername(e.target.value);
-        //         }}
-        //         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-        //         />
-        //   </div>
-        //   <div>
-        //     <label htmlFor="">Full Name<span className="text-red-500">*</span></label>
-        //     <input
-        //       type="text"
-             
-        //       required
-        //       value={fullname}
-        //       onChange={(e) => {
-        //           setfullname(e.target.value);
-        //         }}
-        //         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-        //         />
-        //   </div>
-        //   <div>
-        //     <label htmlFor="">E-mail<span className="text-red-500">*</span></label>
-        //     <input
-        //       type="email"
-              
-        //       value={email}
-        //       onChange={(e) => setEmail(e.target.value)}
-        //       required
-        //       className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-        //     />
-        //   </div>
-        //   <div>
-        //     <label htmlFor="">Password<span className="text-red-500">*</span></label>
-        //     <input
-        //       type="password"
-             
-        //       required
-        //       value={password}
-        //       onChange={(e) => setPassword(e.target.value)}
-        //       className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-        //       />
-        //   </div>
-
-        //   <div className="w-full my-2 rounded-lg flex justify-between">
-        //     <div className="w-1/2 border rounded-lg ">
-        //       <button
-        //         className="w-full rounded-lg px-4 py-2 bgblue"
-        //         ref={user_ref}
-        //         onClick={() => {
-        //             setAdmin(false);
-        //         }}
-        //         >
-        //         {" "}
-        //         User
-        //       </button>
-        //     </div>
-
-        //     <div className="w-1/2 border rounded-lg ">
-        //       <button
-        //         className="w-full rounded-lg px-4 py-2 "
-        //         ref={admin_ref}
-        //         onClick={() => {
-        //             alert("Not allowed to Signup as Admin");
-        //         }}
-        //         >
-        //         Admin
-        //       </button>
-        //     </div>
-        //   </div>
-        //   <button
-        //     type="submit"
-        //     className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
-        //     >
-        //     Signup
-        //   </button>
-        // </form> </div>
-}
+        )}
       </div>
     </>
   );
--- src/components/AuthOTP.jsx
+++ src/components/AuthOTP.jsx
@@ -8,9 +8,12 @@ import { useNavigate } from "react-router-dom";
 export default function AuthOTP({ userData }) {
   const [otp, setOtp] = useState();
   const navigate = useNavigate();
+  const [loading,setLoading] = useState(false);
 
 
-  const handleVerify = async () => {
+  const handleVerify = async (e) => {
+    setLoading(true);
+    e.preventDefault();
     const result = await axios({
             method: "post",
             url: "https://prevyea-server.vercel.app/verify",
@@ -22,7 +25,9 @@ export default function AuthOTP({ userData }) {
           
           if (success) { 
             updateDb();
+           
           }else {
+            setLoading(false);
             toast.error(message);
             
             setTimeout(()=>{navigate("/signup");}, 1000);
@@ -39,13 +44,13 @@ export default function AuthOTP({ userData }) {
     });
             
     const { success, message } = result.data;
-    console.log("Here",success,message);
     
     if (success) {
-            
+      setLoading(false);      
       toast.success(message);
       setTimeout(()=>{navigate("/dashboard");}, 2000);
     } else {
+      setLoading(false);
       setTimeout(()=>{navigate("/signup");}, 1000);
       toast.error(message);
     }
@@ -53,17 +58,48 @@ export default function AuthOTP({ userData }) {
   return (
     <>
       <div>
-        <ToastContainer/>
-        <div className="flex m-8 my-16 gap-4 mt-20 justify-center text-center ">
-          <div>
-            <img src={logo3} />
+      <div className="max-w-md mx-auto text-center bg-white px-4 sm:px-8 py-10 rounded-xl shadow">
+         <div>
+            <img src={logo3} width={256} className="m-auto" />
           </div>
-          <div className="w-full m-auto my-4 border-2 rounded-lg p-4 text-center ">
-            <p className="mt-6 font-bold">
+    <header className="mb-8 mt-8">
+        <h1 className="text-2xl font-bold mb-1">Email Verification</h1>
+        <p className="text-[15px] text-slate-500">Enter the 6-digit verification code that was sent to your Email ID : {userData.email}</p>
+    </header>
+    <form>
+        <div className="flex items-center justify-center gap-3">
+            <input
+                type="text"
+                className="w-60 h-14 text-center text-2xl font-extrabold text-slate-900 bg-slate-100 border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                pattern="\d*" maxLength="6" value={otp} onChange={(e) => {
+                  setOtp(e.target.value);
+                }} />
+            
+        </div>
+        <div className="max-w-[260px] mx-auto mt-4">
+            <button type="submit"
+                className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150"
+                onClick={handleVerify}
+                >Verify
+                Account</button>
+        </div>
+    </form>
+    <div className="text-sm text-slate-500 mt-4">Didn't receive code? <a className="font-medium text-indigo-500 hover:text-indigo-600" href="#0">Resend</a></div>
+</div>
+
+
+
+        <ToastContainer/>
+     
+          {/* <div classNameName="bggreen w-full m-auto my-4 border-2 rounded-lg p-4 text-center ">
+          <div classNameName="bg-white rounded-lg">
+            <img src={logo3} width={350} />
+          </div>
+            <p classNameName="mt-6 font-bold">
               Enter the 6-Digit OTP sent {userData.email}
             </p>
 
-            <div className="w-2/3 flex justify-between m-auto my-4 gap-1.5 ">
+            <div classNameName="w-2/3 flex justify-between m-auto my-4 gap-1.5 ">
               <input
                 type="text"
                 name=""
@@ -73,18 +109,18 @@ export default function AuthOTP({ userData }) {
                 onChange={(e) => {
                   setOtp(e.target.value);
                 }}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
+                classNameName="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300 bg-amber-50"
               />
               <button
                 onClick={handleVerify}
-                className="w-full bgblue py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300"
+                classNameName="w-full bgblue py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 transition duration-300"
               >
                 Verify OTP
               </button>
             </div>
           </div>
+          */}
         </div>
-      </div>
     </>
   );
 }
